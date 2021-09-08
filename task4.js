95eb2112db20756f577c64f05bed429f0db0b81f
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-09-09 00:38:40 +0700

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,19 @@
+let data = {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    address: 
+    {
+    street: "Kulas Light",
+    suite: "Apt. 556",
+    city: "Gwenborough",
+    zipcode: "92998-3874",
+    },
+    phone: "1-770-736-8031 x56442",
+    website: "hildegard.org",
+    }
+
+let newData = {...data, name : 'Fachri maulana', email : 'fachryfachry1997@gmail.com',hobby : ['Basket', 'Music', 'pushrank']
+}
+console.log (newData);
