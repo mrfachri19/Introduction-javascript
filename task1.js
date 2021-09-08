bd802507b15db5e3b027c695970ef91e52435013
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-09-09 00:27:39 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,18 @@
+//soal no.1
+let biodata = {
+    nama : 'fachri maulana',
+    umur : 24,
+    hobi : ['olahraga', 'ngegames',],
+    statusPernikahan : false,
+    daftarSekolah : { 
+        sd : 'SD F. Tandean',
+        smp : 'SMPN 1. Tebing Tinggi',
+        sma : 'SMAN 1. Tebing Tinggi'
+    },
+    keahlian : {
+        photoshop : 'advanced',
+        javascript : 'beginner',
+        PHP : 'beginner'
+    },
+    tertarikCoding : true
+}
