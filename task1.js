e71bad3b75bc3412accf295f836f5d36472ac376
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-09-09 16:25:41 +0700

    Update task1.js

--- task1.js
+++ task1.js
@@ -4,15 +4,39 @@ let biodata = {
     umur : 24,
     hobi : ['olahraga', 'ngegames',],
     statusPernikahan : false,
-    daftarSekolah : { 
-        sd : 'SD F. Tandean',
-        smp : 'SMPN 1. Tebing Tinggi',
-        sma : 'SMAN 1. Tebing Tinggi'
-    },
-    keahlian : {
-        photoshop : 'advanced',
-        javascript : 'beginner',
-        PHP : 'beginner'
-    },
+    schoolList: [
+        {
+          name: 'SD F. Tandean',
+          yearIn: '2003',
+          yearOut: '2019',
+          major: null,
+        },
+        {
+          name: 'SMPN 1 Tebing Tinggi',
+          yearIn: '2009',
+          yearOut: '2012',
+          major: null,
+        },
+        {
+          name: 'SMAN 1 Tebing Tinggi',
+          yearIn: '2012',
+          yearOut: '2015',
+          major: 'ipa',
+        },
+      ],
+      skills: [
+        {
+          skillName: "Phoshop",
+          level: "advanced",
+        },
+        {
+          skillName: "javascript",
+          level: "beginner",
+        },
+        {
+          skillName: "PHP",
+          level: "beginner",
+        },
+      ],
     tertarikCoding : true
 }
